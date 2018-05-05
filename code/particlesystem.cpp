6f6f9fc4b8f8e4146f6dd3a572e046b2e3bcd498
yjmantilla <yjmantilla@gmail.com>
2018-05-05 00:09:46 -0500

    system 2 working

--- code/particlesystem.cpp
+++ code/particlesystem.cpp
@@ -47,17 +47,29 @@ void ParticleSystem::updateParticleAcceleration(unsigned int i)
     double dummyAngle;
     dummyPi=this->systemParticles.at(i);
 
+    /*include the effect over particle i of all the other particles*/
     for(unsigned int j=0;j<this->systemParticles.size();j++)
     {
-        if(j==i){continue;}
+        if(j==i){continue;}//same particle, ignore
         else
         {
+            /*auxiliar dummy variables so expresions are shorter, just for readability*/
+
             dummyPj=this->systemParticles.at(j);
-            dummyAcceleration=this->getG()*dummyPj.getMass()/pow(distanceBetweenParticles(dummyPi,dummyPj),2);
+
+            /*a_j->i = G*Mj/r^2*/
+            dummyAcceleration=this->getG()*dummyPj.getMass()/pow(distanceBetweenParticles(dummyPi,dummyPj),2); //same for both components
+
+            /*calculate angle to decompose the a vector*/
+            /*order of atan2 is important, it changes the points completely*/
             dummyAngle = this->angleBetweenParticles(dummyPi,dummyPj);// why is this the correct order though? pj - pi
+
+            /*For Debug*/
 //            std::cout<<this->angleBetweenParticles(dummyPi,dummyPj)<<std::endl;
 //            std::cout<<this->angleBetweenParticles(dummyPj,dummyPi)<<std::endl;
 
+            /*we add to the accumulated acceleration (from previous particles) the current particle acceleration*/
+            /*multiply by sin and cos for decomposition into x (costheta) and y (sintheta) components*/
             this->systemParticles.at(i).setXAcceleration(this->systemParticles.at(i).getXAcceleration()+dummyAcceleration*cos(dummyAngle));
             this->systemParticles.at(i).setYAcceleration(this->systemParticles.at(i).getYAcceleration()+dummyAcceleration*sin(dummyAngle));
         }
@@ -76,6 +88,7 @@ void ParticleSystem::updateAccelerations()
 
 void ParticleSystem::updateParticleVelocity(unsigned int i)
 {
+    /*v=v0 + a*deltat*/
     this->systemParticles.at(i).setXVelocity(this->systemParticles.at(i).getXVelocity()+this->systemParticles.at(i).getXAcceleration()*this->timeStep);
     this->systemParticles.at(i).setYVelocity(this->systemParticles.at(i).getYVelocity()+this->systemParticles.at(i).getYAcceleration()*this->timeStep);
 }
@@ -91,6 +104,7 @@ void ParticleSystem::updateVelocities()
 
 void ParticleSystem::updateParticlePosition(unsigned int i)
 {
+    /* r = r0 + v0*deltat + 0.5*a*deltat^2 */
     this->systemParticles.at(i).setXPosition(this->systemParticles.at(i).getXPosition() + (this->systemParticles.at(i).getXVelocity()*this->timeStep)+ (this->systemParticles.at(i).getXAcceleration()*pow(timeStep,2)*0.5));
     this->systemParticles.at(i).setYPosition(this->systemParticles.at(i).getYPosition() + (this->systemParticles.at(i).getYVelocity()*this->timeStep)+ (this->systemParticles.at(i).getYAcceleration()*pow(timeStep,2)*0.5));
     return;
@@ -122,11 +136,11 @@ void ParticleSystem::evolveSystem(unsigned int numberOfSteps)
 
     for(unsigned int i = 0; i < numberOfSteps; i++)
     {
-
-        this->updatePositions();
-        this->updateVelocities();        
-        this->deleteAccelerations();
-        this->updateAccelerations();
+        //this order is important
+        this->updatePositions();    //calculate initial positions of next state
+        this->updateVelocities();    //calculate initial velocities of next state
+        this->deleteAccelerations(); //if we dont do this the accelerations get integrated (which is incorrect)
+        this->updateAccelerations(); //calculate accelerations for the next state
         this->time=this->time+this->timeStep;
     }
 
@@ -135,6 +149,8 @@ void ParticleSystem::evolveSystem(unsigned int numberOfSteps)
 std::string ParticleSystem::SystemState()
 {
     std::string str;
+
+    //basically time,x0,y0,x1,y1...xn,yn where the index is the particle number
 
     str.append(std::to_string(this->time));
     str.append(",");
@@ -147,7 +163,7 @@ std::string ParticleSystem::SystemState()
         str.append(",");
     }
 
-    str.pop_back();
+    str.pop_back();//delete last ','
     return str;
 }
 
@@ -155,7 +171,7 @@ void ParticleSystem::saveSystemStatesToFile(std::string filename, unsigned int n
 {
         std::fstream ofs(filename,std::ifstream::out| std::fstream::trunc);
 
-
+        /*headers of columns*/
         std::string str;
         str.append("time,");
         for(unsigned int i = 0; i < this->systemParticles.size(); i++)
@@ -168,16 +184,18 @@ void ParticleSystem::saveSystemStatesToFile(std::string filename, unsigned int n
             str.append(",");
         }
 
-        str.pop_back();
+        str.pop_back(); //delete last ','
 
         ofs<<str<<std::endl;
 
+        /*Print Initial State*/
         ofs<<this->SystemState()<<std::endl;
 
+        /*Evolution of the system*/
         for (unsigned int i = 0; i < numberOfStates; i++)
         {
         this->evolveSystem(1);
-        ofs<<this->SystemState();
+        ofs<<this->SystemState(); //print current state
         if(i==numberOfStates-1){continue;}//last with no endline
         ofs<<std::endl;
         }
